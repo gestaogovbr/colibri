c5759b9fe66496c2b5fe59ba5a1ab54405bae241
Arthur Delpino Barbabella <arthur.barbabella@gestao.gov.br>
2026-05-26 10:57:22 -0300

    fix: skip anos carregados e bucket correto no NCM

--- ingestion/pncp/pipeline.py
+++ ingestion/pncp/pipeline.py
@@ -1,4 +1,4 @@
-from ingestion.pncp import load
+from ingestion.pncp.bronze import extract_compra, extract_compra_item, extract_item_resultado
 
 
 NOME_SEGREDO = "colibri-token-desenvolvedor"
@@ -7,13 +7,23 @@ CAMINHO_META = f"s3://{BUCKET}/meta.ducklake"
 DATA_PATH = f"s3://{BUCKET}/lake/"
 ANOS = range(2021, 2027)
 
-VIEWS = [
+VIEWS_COMPRA = [
     "VW_FT_PNCP_COMPRA",
+]
+
+VIEWS_COMPRA_ITEM = [
+    "VW_FT_PNCP_COMPRA_ITEM",
+]
+
+VIEWS_ITEM_RESULTADO = [
+    "VW_DM_PNCP_ITEM_RESULTADO",
 ]
 
 
 def main():
-    load.main(VIEWS, ANOS, CAMINHO_META, DATA_PATH, NOME_SEGREDO)
+    extract_compra.main(VIEWS_COMPRA, ANOS, CAMINHO_META, DATA_PATH, NOME_SEGREDO)
+    extract_compra_item.main(VIEWS_COMPRA_ITEM, ANOS, CAMINHO_META, DATA_PATH, NOME_SEGREDO)
+    extract_item_resultado.main(VIEWS_ITEM_RESULTADO, ANOS, CAMINHO_META, DATA_PATH, NOME_SEGREDO)
 
 
 if __name__ == "__main__":
