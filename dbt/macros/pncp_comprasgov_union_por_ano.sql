a76d878d0e94d43cb604da2ad71f550732fb694e
heitorgama <heitor.gama@gmail.com>
2026-06-30 13:14:05 -0300

    Macro dbt: Substituir função que lê CSV pela que lê Parquet

--- dbt/macros/pncp_comprasgov_union_por_ano.sql
+++ dbt/macros/pncp_comprasgov_union_por_ano.sql
@@ -3,7 +3,7 @@
     * EXCLUDE (filename),
     regexp_extract(filename, 'comprasGOV-(\w+)-{{ view }}', 1) AS granularidade,
     regexp_extract(filename, '{{ view }}-(.+)\.csv$', 1) AS periodo
-  FROM read_csv_auto(
+  FROM read_parquet(
     [
       's3://{{ var("bucket_lake") }}/pncp_comprasgov_diario/*/*/*/comprasGOV-diario-{{ view }}-*.parquet',
       's3://{{ var("bucket_lake") }}/pncp_comprasgov_mensal/*/*/comprasGOV-mensal-{{ view }}-*.parquet',
