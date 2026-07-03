f5e5ed6ce7a678d27b26233ff5bcfc45e2c664ff
Arthur Delpino Barbabella <arthur.barbabella@gestao.gov.br>
2026-07-03 12:10:12 -0300

    Adiciona modelo stg para tabela dimensão das margens
    Inclui resoluções da CICS e CIIA-PAC

--- dbt/modelos/staging/margem/stg_dim_margem_ncm.sql
+++ dbt/modelos/staging/margem/stg_dim_margem_ncm.sql
@@ -1,7 +1,11 @@
 {{ config(
     materialized='table',
     database='lake',
-    tags=['staging', 'ncm']
+    tags=['staging', 'margem']
 ) }}
 
-SELECT * FROM read_csv_auto('../dados/dim_margem_ncm_utf8.csv', header = true)
+-- Cruza a tabela de prefixos por período com a tabela de NCM pra ter o código completo do NCM
+SELECT ncm.codigo, d.* EXCLUDE (prefixo_ncm)
+FROM {{ ref('stg_dim_margem_ncm_prefixos') }} AS d
+JOIN read_csv('../dados/ncm_prefixos.csv') AS ncm
+    ON ncm.prefixo = d.prefixo_ncm
