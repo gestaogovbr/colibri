f1d1dda86220e75770ec9d9c270bd96fb93d6aa3
Arthur Delpino Barbabella <arthur.barbabella@gestao.gov.br>
2026-07-15 13:34:13 -0300

    Ajusta as macros ao novo formato das origens

--- dbt/macros/margem_eventos_cics.sql
+++ dbt/macros/margem_eventos_cics.sql
@@ -45,7 +45,7 @@
         round(TRY_CAST(trim(margem_normal)         AS DOUBLE) / 100, 4)  AS margem_normal_pct,
         NULL::VARCHAR                                                    AS margem_adicional_comprovante,
         NULL::DECIMAL(3,2)                                               AS margem_adicional_pct,
-        trim(grupo_de_produtos)                                                 AS grupo_de_produtos
+        trim(grupo_de_produtos)                                          AS grupo_de_produtos
     FROM read_xlsx('{{ path }}', sheet='{{ aba_anterior }}', all_varchar=true)
     WHERE trim(resolucao) = '{{ anterior }}' AND trim(ncm) IS NOT NULL AND trim(ncm) != ''
 
@@ -60,7 +60,7 @@
         round(TRY_CAST(trim(margem_normal)         AS DOUBLE) / 100, 4)  AS margem_normal_pct,
         replace(trim(regra_de_qualificacao), ' ', '_')                   AS margem_adicional_comprovante,
         round(TRY_CAST(trim(margem_adicional)      AS DOUBLE) / 100, 4)  AS margem_adicional_pct,
-        trim(grupo_de_produtos)                                                 AS grupo_de_produtos
+        trim(grupo_de_produtos)                                          AS grupo_de_produtos
     FROM read_xlsx('{{ path }}', sheet='{{ aba_anterior }}', all_varchar=true)
     WHERE trim(resolucao) = '{{ anterior }}' AND trim(ncm) IS NOT NULL AND trim(ncm) != ''
         AND trim(margem_adicional) IS NOT NULL AND trim(margem_adicional) != ''
--- dbt/macros/margem_eventos_ciiapac.sql
+++ dbt/macros/margem_eventos_ciiapac.sql
@@ -10,7 +10,7 @@
         round(TRY_CAST(trim(margem_normal) AS DOUBLE), 4)                              AS margem_normal_pct,
         NULL::VARCHAR                                                                  AS margem_adicional_comprovante,
         NULL::DECIMAL(3,2)                                                             AS margem_adicional_pct,
-        trim(anexo)                                                                    AS grupo_de_produtos
+        trim(grupo_de_produtos)                                                        AS grupo_de_produtos
     FROM read_xlsx('{{ path }}', sheet='{{ aba }}', all_varchar=true)
     WHERE trim(ncm) IS NOT NULL AND trim(ncm) != ''
         AND trim(margem_normal) IS NOT NULL AND trim(margem_normal) != ''
@@ -26,7 +26,7 @@
         round(TRY_CAST(trim(margem_normal) AS DOUBLE), 4)                              AS margem_normal_pct,
         replace(trim(regra_de_qualificacao), ' ', '_')                                 AS margem_adicional_comprovante,
         round(TRY_CAST(trim(margem_adicional) AS DOUBLE), 4)                           AS margem_adicional_pct,
-        trim(anexo)                                                                    AS grupo_de_produtos
+        trim(grupo_de_produtos)                                                        AS grupo_de_produtos
     FROM read_xlsx('{{ path }}', sheet='{{ aba }}', all_varchar=true)
     WHERE trim(ncm) IS NOT NULL AND trim(ncm) != ''
         AND trim(margem_adicional) IS NOT NULL AND trim(margem_adicional) != ''
